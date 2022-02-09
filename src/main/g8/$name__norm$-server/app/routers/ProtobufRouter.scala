aecac1516da05eb90d239b7ce4772ba29cbc4034
Enseart Alwyn Simpson <enzeart@gmail.com>
2022-02-08 19:03:16 -0500

    Import core service into service router

--- src/main/g8/$name__norm$-server/app/routers/ProtobufRouter.scala
+++ src/main/g8/$name__norm$-server/app/routers/ProtobufRouter.scala
@@ -3,14 +3,16 @@ package routers
 import akka.actor.ActorSystem
 import akka.grpc.scaladsl.Metadata
 import $package$.grpc.{Abstract$name;format="space,Camel"$ServicePowerApiRouter, EchoReply, EchoRequest}
+import $package$.$name;format="space,Camel"$Service
 import config.AppConfig
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.Future
 
 @Singleton
-class ProtobufRouter @Inject() (appConfig: AppConfig)(implicit actorSystem: ActorSystem)
-    extends Abstract$name;format="space,Camel"$ServicePowerApiRouter(actorSystem) {
+class ProtobufRouter @Inject() (appConfig: AppConfig, $name;format="space,camel"$Service: $name;format="space,Camel"$Service)(
+    implicit actorSystem: ActorSystem
+) extends Abstract$name;format="space,Camel"$ServicePowerApiRouter(actorSystem) {
 
   override def echo(in: EchoRequest, metadata: Metadata): Future[EchoReply] =
     Future.successful(EchoReply(message = in.message))
