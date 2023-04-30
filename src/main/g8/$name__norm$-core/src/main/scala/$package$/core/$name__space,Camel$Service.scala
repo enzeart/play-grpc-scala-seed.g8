43e66a770f7c94d279c6495c2e8a955056e19fd5
Enseart Alwyn Simpson <enzeart@gmail.com>
2023-04-30 16:06:14 -0400

    Remove default serverice template

--- src/main/g8/$name__norm$-core/src/main/scala/$package$/core/$name__space,Camel$Service.scala
+++
@@ -1,9 +0,0 @@
-package $package$.core
-
-import javax.inject.{Inject, Singleton}
-import scala.concurrent.ExecutionContext
-
-@Singleton()
-class $name;format="space,Camel"$Service @Inject() ()(implicit executionContext: ExecutionContext) {
-
-}
--- src/main/g8/$name__norm$-server/app/routers/ProtobufRouter.scala
+++ src/main/g8/$name__norm$-server/app/routers/ProtobufRouter.scala
@@ -3,14 +3,13 @@ package routers
 import akka.actor.ActorSystem
 import akka.grpc.scaladsl.Metadata
 import $package$.grpc.{Abstract$name;format="space,Camel"$ServicePowerApiRouter, EchoReply, EchoRequest}
-import $package$.core.$name;format="space,Camel"$Service
 import config.AppConfig
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class ProtobufRouter @Inject() (appConfig: AppConfig, $name;format="space,camel"$Service: $name;format="space,Camel"$Service)(
+class ProtobufRouter @Inject() (appConfig: AppConfig)(
     implicit actorSystem: ActorSystem,
     executionContext: ExecutionContext
 ) extends Abstract$name;format="space,Camel"$ServicePowerApiRouter(actorSystem) {
