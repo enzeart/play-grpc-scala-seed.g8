e14667add1e18a229a17459ed948906b811f9dd5
Enseart Alwyn Simpson <enzeart@gmail.com>
2025-03-10 16:31:19 -0400

    Added GrpcServiceHelpers

--- src/main/g8/$name__norm$-server/app/routers/$name__space,Camel$ServiceRouter.scala
+++ src/main/g8/$name__norm$-server/app/routers/$name__space,Camel$ServiceRouter.scala
@@ -4,6 +4,7 @@ import org.apache.pekko.actor.ActorSystem
 import org.apache.pekko.grpc.scaladsl.Metadata
 import $package$.grpc.{Abstract$name;format="space,Camel"$ServicePowerApiRouter, EchoReply, EchoRequest}
 import config.AppServerConfig
+import play.api.Logging
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
@@ -11,9 +12,12 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class $name;format="space,Camel"$ServiceRouter @Inject() (appServerConfig: AppServerConfig)(
     implicit actorSystem: ActorSystem,
-    executionContext: ExecutionContext
-) extends Abstract$name;format="space,Camel"$ServicePowerApiRouter(actorSystem) {
+    override val executionContext: ExecutionContext
+) extends Abstract$name;format="space,Camel"$ServicePowerApiRouter(actorSystem)
+    with GrpcServiceHelpers
+    with Logging {
 
-  override def echo(in: EchoRequest, metadata: Metadata): Future[EchoReply] =
+  override def echo(in: EchoRequest, metadata: Metadata): Future[EchoReply] = withGrpcServiceExceptionHandling {
     Future.successful(EchoReply(message = in.message))
+  }
 }
